c07be4d2910a4b1c2ce99465ab9431db12c510c9
Matheus Andrioli <matheus_andrioli@hotmail.com>
2021-04-12 20:27:14 -0300

    feature: Validates active subscription

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -5,6 +5,7 @@ import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -17,7 +18,7 @@ interface PostsProps {
   posts: Post[];
 }
 
-export default function Posts({ posts }) {
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -27,11 +28,13 @@ export default function Posts({ posts }) {
       <main className={styles.container} >
         <div className={styles.posts} >
           { posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`} >
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           )) }
         </div>
       </main>
