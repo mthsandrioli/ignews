4a4fdbe56c913d219f1a19c2382f9e4df6dcda19
Matheus Andrioli <matheus_andrioli@hotmail.com>
2021-04-12 23:09:44 -0300

    feature: Post preview page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -5,7 +5,6 @@ import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
 
 import styles from './post.module.scss';
-import { Session } from "next-auth";
 
 interface PostsProps {
   post: {
@@ -41,7 +40,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req });
   const { slug } = params;
 
-  if(!session.activeSubscription) {
+  if(!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
